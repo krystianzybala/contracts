6b35b720add076cca316beb27908bcbc3391c5a7
Krystian Zybala <kontakt@kzybala.pl>
2018-04-12 18:05:07 +0200

    Add method to send message.

--- src/main/java/pl/krystianzybala/contract/controller/FraudDetectionController.java
+++ src/main/java/pl/krystianzybala/contract/controller/FraudDetectionController.java
@@ -2,8 +2,12 @@ package pl.krystianzybala.contract.controller;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import org.springframework.cloud.stream.messaging.Source;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,12 +15,24 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class FraudDetectionController {
 
+    private final Source source;
+
+    public FraudDetectionController(final Source source) {
+        this.source = source;
+    }
+
     @PutMapping(path = "/fraudcheck", consumes = MediaType.APPLICATION_JSON_UTF8_VALUE, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
     @ResponseStatus(code = HttpStatus.OK)
     public Response check(final Fraud fraud) {
-
-
         return new Response("FRAUD", "Amount too high");
+    }
+
+    public void message() {
+        this.source.output().send(MessageBuilder
+        .withPayload(
+                new FraudMessage("Krystian send message!")
+        )
+        .build());
     }
 
     @AllArgsConstructor
@@ -34,4 +50,12 @@ public class FraudDetectionController {
 class Response {
     private String fraudCheckStatus;
     private String rejectionReason;
+}
+
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Data
+class FraudMessage {
+    private String message;
 }
