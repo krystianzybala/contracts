ffbac73022cc4697aac4d8ef5da4a47eeaec26d9
Krystian Zybala <kontakt@kzybala.pl>
2018-04-12 18:05:26 +0200

    Add trigger method which send message to broker.

--- src/test/java/pl/krystianzybala/contract/BaseClass.java
+++ src/test/java/pl/krystianzybala/contract/BaseClass.java
@@ -3,16 +3,27 @@ package pl.krystianzybala.contract;
 import io.restassured.module.mockmvc.RestAssuredMockMvc;
 import org.junit.Before;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.contract.verifier.messaging.boot.AutoConfigureMessageVerifier;
 import org.springframework.test.context.junit4.SpringRunner;
 import pl.krystianzybala.contract.controller.FraudDetectionController;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
-
+@AutoConfigureMessageVerifier
 public class BaseClass {
+
+    @Autowired
+    FraudDetectionController fraudDetectionController;
 
     @Before
     public void setup() {
-        RestAssuredMockMvc.standaloneSetup(new FraudDetectionController());
+        RestAssuredMockMvc.standaloneSetup(fraudDetectionController);
+    }
+
+    public void triggerMessage() {
+
+        this.fraudDetectionController.message();
     }
 }
